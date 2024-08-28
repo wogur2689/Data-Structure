817c15d4f0867ef2efb0c85022f16ebcaf77210a
길재혁 <81075170+wogur2689@users.noreply.github.com>
2024-08-28 23:53:46 +0900

    Create stack.py

---
+++ stack.py
@@ -0,0 +1,14 @@
+# 1차원 배열 리스트로 구현시
+data = [1, 2, 3, 4, 5]
+print(data)
+
+# 2차원 배열 리스트로 구현시
+data = [[1, 2, 3], [4, 5, 6] [7, 8, 9]]
+print(data)
+
+print(data[0])
+
+print(data[0][0])
+print(data[0][1])
+print(data[0][2])
+print(data[1][0])
