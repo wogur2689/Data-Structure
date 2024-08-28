4d3156afe919d9ff992002f3f2ca4f1d522ea67b
길재혁 <81075170+wogur2689@users.noreply.github.com>
2024-08-28 23:54:03 +0900

    Update stack.py

--- stack.py
+++ stack.py
@@ -12,3 +12,46 @@ print(data[0][0])
 print(data[0][1])
 print(data[0][2])
 print(data[1][0])
+
+# 연습 1 : 위의 2차원 배열에서 9, 8, 7 순서로 출력해보기
+print(data[2][2], data[2][1], data[2][0])
+
+# 연습 2 : 다음 dataSet에서 전체 이름안에 M이 몇번 남았는지
+dataset = ['Braund, Mr. Owen Harris',
+'Cumings, Mrs. John Bradley (Florence Briggs Thayer)',
+'Heikkinen, Miss. Laina',
+'Futrelle, Mrs. Jacques Heath (Lily May Peel)',
+'Allen, Mr. William Henry',
+'Moran, Mr. James',
+'McCarthy, Mr. Timothy J',
+'Palsson, Master. Gosta Leonard',
+'Johnson, Mrs. Oscar W (Elisabeth Vilhelmina Berg)',
+'Nasser, Mrs. Nicholas (Adele Achem)',
+'Sandstrom, Miss. Marguerite Rut',
+'Bonnell, Miss. Elizabeth',
+'Saundercock, Mr. William Henry',
+'Andersson, Mr. Anders Johan',
+'Vestrom, Miss. Hulda Amanda Adolfina',
+'Hewlett, Mrs. (Mary D Kingcome) ',
+'Rice, Master. Eugene',
+'Williams, Mr. Charles Eugene',
+'Vander Planke, Mrs. Julius (Emelia Maria Vandemoortele)',
+'Masselmani, Mrs. Fatima',
+'Fynney, Mr. Joseph J',
+'Beesley, Mr. Lawrence',
+'McGowan, Miss. Anna "Annie"',
+'Sloper, Mr. William Thompson',
+'Palsson, Miss. Torborg Danira',
+'Asplund, Mrs. Carl Oscar (Selma Augusta Emilia Johansson)',
+'Emir, Mr. Farred Chehab',
+'Fortune, Mr. Charles Alexander',
+'Dwyer, Miss. Ellen "Nellie"',
+'Todoroff, Mr. Lalio']
+
+m_count = 0
+for data in dataset:
+  for index in range(len(data)):
+    if data[index] == 'M':
+      m_count += 1
+
+print(m_count)
